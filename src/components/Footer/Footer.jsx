38854ed8190c5e5f75f5fc91a89d4474aa73a19e
Lauren Tanea <taneajoshi@gmail.com>
2022-06-10 10:47:07 +0530

    added footer component to the home pae and styled it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,17 +7,62 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import './Footer.scss';
 
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  p {
+    max-width: 75%;
+  }
+`;
+const Center = styled.div`
+  h3 {
+    margin-bottom: 15px;
+  }
+  ul {
+    padding: 0;
+    list-style: none;
+    column-gap: 98px;
+
+    li {
+      text-transform: capitalize;
+      cursor: pointer;
+      margin: 5px 0;
+
+      &:hover {
+        text-decoration: underline;
+        text-underline-offset: 1px;
+        color: teal;
+      }
+    }
+  }  
+`;
+
 const PhoneNumber = styled.div``;
 const Email = styled.div``;
 
+const Right = styled.div`
+  svg {
+    color: #008080;
+    font-size: 20px;
+    margin-right: 8px;
+  }
+
+  h3 {
+    margin-bottom: 15px;
+  }
+
+  address,
+  ${PhoneNumber}, 
+  ${Email} {
+    margin-bottom: 10px;
+  }
+`;
+
+
 const Footer = () => {
   return (
-    <footer className='flex-center'>
-      <Left className="flex-1">
+    <footer className='justify-center'>
+      <Left className="col-center flex-1">
         <div className="footer-logo">Zudit</div>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt ex et eveniet similique porro temporibus quo dolorum minima veniam! Veniam?</p>
         <div className="social-links">
@@ -27,9 +72,9 @@ const Footer = () => {
           <PinterestIcon />
         </div>
       </Left>
-      <Center className="flex-1">
+      <Center className="col-center flex-1">
         <h3>Useful Links</h3>
-        <ul className="col-count-2">
+        <ul className="col-count-3">
           <li>Home</li>
           <li>man fashion</li>
           <li>accessories</li>
@@ -42,11 +87,11 @@ const Footer = () => {
           <li>terms</li>
         </ul>
       </Center>
-      <Right className="flex-1">
+      <Right className="col-center flex-1">
         <h3>Contact</h3>
-        <address><LocationOnOutlinedIcon/>D22 Dixie path, Newsberg</address>
-        <PhoneNumber><LocalPhoneOutlinedIcon/> <a href="tel:12 243-23-234">+12 243-23-234</a></PhoneNumber>
-        <Email><MarkEmailUnreadOutlinedIcon /><a href="mailto:abc@gmail.com">abc@gmail.com</a></Email>
+        <address className="align-center"><LocationOnOutlinedIcon/>D22 Dixie path, Newsberg</address>
+        <PhoneNumber className="align-center"><LocalPhoneOutlinedIcon/> <a href="tel:12 243-23-234">+12 243-23-234</a></PhoneNumber>
+        <Email className="align-center"><MarkEmailUnreadOutlinedIcon /><a href="mailto:abc@gmail.com">abc@gmail.com</a></Email>
       </Right>
     </footer>
   )
