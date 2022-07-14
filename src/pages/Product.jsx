74f2b99ca6c4ca074859751493aabc6bc6804387
Lauren Tanea <taneajoshi@gmail.com>
2022-07-14 13:10:37 +0530

    worked on product page changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@mui/icons-material';
 import React from 'react'
 import styled from 'styled-components';
 import Footer from '../components/Footer/Footer';
@@ -16,7 +17,9 @@ const ImgContainer = styled.div`
     marign-right: 50px;
 `;
 
-const InfoContainer = styled.div``;
+const InfoContainer = styled.div`
+    padding: 0 50px;
+`;
 
 const Image = styled.img`
     height: 100%;
@@ -25,13 +28,63 @@ const Image = styled.img`
 `;
 
 const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 500;`;
+
+const Desc = styled.p`
+    font-size: 22px;
+`;
+
+const Price = styled.div`
     font-size: 40px;
-    font-weight: 300;`
-;
+    font-weight: 100;
+    margin-top: 30px;
+`;
 
-const Desc = styled.p``;
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0;
+`;
+const Filter = styled.div``;
 
-const Price = styled.div``;
+const FilterTitle = styled.span`
+font-size: 20px;
+font-weight: 200;`;
+
+const FilterColor = styled.div`
+   height: 20px;
+   width: 20px;
+   border-radius: 50%;
+   background-color: ${props => props.color};
+   margin: 0 6px;
+   cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+    width: 80px;
+    min-height: 40px;
+    margin-left: 12px;`;
+
+const FilterSizeOption = styled.option``;
+
+const AddWrapper = styled.div`
+    max-width: 50%;
+    justify-content: space-between;
+`;
+
+const AmountWrapper = styled.div`
+    min-width: 100px;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    height: 28px;
+    width: 28px;
+    border: 2px solid #000000;
+    border-radius: 4px;
+`;
+
+const Button = styled.button``;
 
 const Product = () => {
   return (
@@ -45,6 +98,32 @@ const Product = () => {
                <Title>Denim Jumpsuit</Title>
                <Desc>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero consectetur ipsum quibusdam provident aliquid maxime eos beatae quae sunt mollitia.</Desc>
                <Price>$ 20</Price>
+               <FilterContainer className="justify-between">
+                   <Filter className='align-center'>
+                       <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="#000000"></FilterColor>
+                        <FilterColor color="darkblue"></FilterColor>
+                        <FilterColor color="#808080"></FilterColor>
+                   </Filter>
+                   <Filter className="align-center">
+                       <FilterTitle>Size</FilterTitle>
+                       <FilterSize>
+                           <FilterSizeOption>XS</FilterSizeOption>
+                           <FilterSizeOption>S</FilterSizeOption>
+                           <FilterSizeOption>M</FilterSizeOption>
+                           <FilterSizeOption>L</FilterSizeOption>
+                           <FilterSizeOption>XL</FilterSizeOption>
+                       </FilterSize>
+                   </Filter>
+               </FilterContainer>
+               <AddWrapper className="align-center">
+                   <AmountWrapper className="align-center justify-between">
+                       <Remove className="cursor-pointer"/>
+                       <Amount className='flex-center'>1</Amount>
+                       <Add className="cursor-pointer"/>
+                   </AmountWrapper>
+                   <Button className="global-btn">ADD TO CART</Button>
+               </AddWrapper>
            </InfoContainer>
        </Wrapper>
        <Newsletter />
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,16 +5,9 @@ import Navbar from '../components/Navbar/Navbar';
 import Newsletter from '../components/Newsletter';
 import Products from '../components/Products/Products';
 
-const Container = styled.div``;
-
-const Title = styled.h1`
-    margin-bottom: 20px`;
-
 const FilterContainer = styled.div``;
 
-const Filter = styled.div`
-    margin-bottom: 20px;
-`;
+const Filter = styled.div``;
 
 const FilterText = styled.h3`
     margin-right: 10px;
@@ -25,44 +18,49 @@ const Select = styled.select`
     padding: 10px 5px;
     min-width: 100px;
     border: 1px solid #d3d3d3;
-    color: #444141;`
-;
+    color: #444141;`;
+
 const Option = styled.option``;
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+    margin-bottom: 20px`;
 
 const ProductList = () => {
   return (
     <Container className='global-container text-center'>
         <Navbar/>
         <Title>Dresses</Title>
-        <FilterContainer className='justify-between'>
-            <Filter className='align-center'>
-                <FilterText>Filter Products</FilterText>
-                <Select>
-                    <Option disabled selected>Color</Option>
-                    <Option>White</Option>
-                    <Option>Black</Option>
-                    <Option>Red</Option>
-                    <Option>Blue</Option>
-                    <Option>Yellow</Option>
-                    <Option>Green</Option>
-                </Select>
-                <Select>
-                    <Option disabled selected>Size</Option>
-                    <Option>XS</Option>
-                    <Option>S</Option>
-                    <Option>M</Option>
-                    <Option>L</Option>
-                    <Option>XL</Option>
-                </Select>
-            </Filter>
-            <Filter className='align-center'>
-                <FilterText>Sort Products</FilterText>
-                <Select>
-                    <Option selected>Newest</Option>
-                    <Option>Low to High</Option>
-                    <Option>High to Low</Option>
-                </Select>
-            </Filter>
+            <FilterContainer className='justify-between'>
+                <Filter className='align-center'>
+                    <FilterText>Filter Products</FilterText>
+                    <Select>
+                        <Option disabled selected>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter className='align-center'>
+                    <FilterText>Sort Products</FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Low to High</Option>
+                        <Option>High to Low</Option>
+                    </Select>
+                </Filter>
         </FilterContainer>
         <Products/>
         <Newsletter/>
